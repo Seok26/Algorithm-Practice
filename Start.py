6cf6e6b8a8b2d60806d6180cf16178ad3108ede0
shino <shinoatsuki@naver.com>
2023-02-18 10:14:37 +0900

    Progrmmers Lv2 JadenCase

--- Start.py
+++ Start.py
@@ -1,2 +1,20 @@
-print("Hello")
-print("This Page Is Git Repository Connect Test Python File")
+#Progrmmers Lever 2. JadenCase
+def solution(s):
+    #시작 시 모든 문자열은 소문자로 변경
+    s = s.lower()
+    li = list(s)
+
+    for i in range(len(li)):
+        #공백이면 건너뛰기
+        if li[i] == " ":
+            continue
+
+        #소문자인 경우
+        if ord('a') <= ord(li[i]) <= ord('z'):
+
+            #JadenCase
+            if li[i - 1] == " " or i == 0:
+                li[i] = li[i].upper()
+
+
+    return ''.join(s for s in li)
